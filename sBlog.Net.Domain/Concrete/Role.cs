9695b576153a740a21961aacb4af4ce8f3c7b683
Karthik Ananthapadmanaban <karthika25@gmail.com>
2013-06-05 20:32:34 -0400

    implement idisposable for the role concrete type and add license header for new files

--- sBlog.Net.Domain/Concrete/Role.cs
+++ sBlog.Net.Domain/Concrete/Role.cs
@@ -1,4 +1,21 @@
-﻿using System.Collections.Generic;
+﻿#region Disclaimer/License Info
+
+/* *********************************************** */
+
+// sBlog.Net
+
+// sBlog.Net is a minimalistic blog engine software.
+
+// Homepage: http://sblogproject.net
+// Github: http://github.com/karthik25/sBlog.Net
+
+// This project is licensed under the BSD license.  
+// See the License.txt file for more information.
+
+/* *********************************************** */
+
+#endregion
+using System.Collections.Generic;
 using System.Data.Linq;
 using System.Linq;
 using sBlog.Net.Domain.Entities;
--- sBlog.Net.Domain/Interfaces/IRole.cs
+++ sBlog.Net.Domain/Interfaces/IRole.cs
@@ -1,9 +1,27 @@
-﻿using System.Collections.Generic;
+﻿#region Disclaimer/License Info
+
+/* *********************************************** */
+
+// sBlog.Net
+
+// sBlog.Net is a minimalistic blog engine software.
+
+// Homepage: http://sblogproject.net
+// Github: http://github.com/karthik25/sBlog.Net
+
+// This project is licensed under the BSD license.  
+// See the License.txt file for more information.
+
+/* *********************************************** */
+
+#endregion
+using System;
+using System.Collections.Generic;
 using sBlog.Net.Domain.Entities;
 
 namespace sBlog.Net.Domain.Interfaces
 {
-    public interface IRole
+    public interface IRole : IDisposable
     {
         List<RoleEntity> GetAllRoles();
         bool IsInRole(int userId, string roleName);
