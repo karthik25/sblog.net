8488925563436172c35a0d74ead2ef10e19a4982
Karthik Ananthapadmanaban <karthika25@gmail.com>
2013-06-19 23:54:25 -0400

    so so working version of globalization/localization

---
+++ sBlog.Net/Controllers/GlobalizeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using System.Web.Mvc;
+
+namespace sBlog.Net.Controllers
+{
+    public class GlobalizeController : Controller
+    {
+        public ActionResult ChangeCulture(string lang)
+        {
+            Session["Culture"] = new CultureInfo(lang);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- sBlog.Net/Global.asax.cs
+++ sBlog.Net/Global.asax.cs
@@ -17,6 +17,8 @@
 #endregion
 using System;
 using System.Data.SqlClient;
+using System.Globalization;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -172,7 +174,35 @@ namespace sBlog.Net
         public override void Init()
         {
             PostAuthenticateRequest += MvcApplication_PostAuthenticateRequest;
+            AcquireRequestState += MvcApplication_OnAcquireRequestState;
             base.Init();
+        }
+
+        protected void MvcApplication_OnAcquireRequestState(object sender, EventArgs e)
+        {
+            //It's important to check whether session object is ready
+            if (HttpContext.Current.Session != null)
+            {
+                var ci = (CultureInfo)Session["Culture"];
+                //Checking first if there is no value in session and set default language
+                //this can happen for first user's request
+                if (ci == null)
+                {
+                    var langName = "en";
+
+                    //Try to get values from Accept lang HTTP header
+                    if (HttpContext.Current.Request.UserLanguages != null && HttpContext.Current.Request.UserLanguages.Length != 0)
+                    {
+                        //Gets accepted list 
+                        langName = HttpContext.Current.Request.UserLanguages[0].Substring(0, 2);
+                    }
+                    ci = new CultureInfo(langName);
+                    Session["Culture"] = ci;
+                }
+
+                Thread.CurrentThread.CurrentUICulture = ci;
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(ci.Name);
+            }
         }
 
         private void SetupDependencyManagement()
