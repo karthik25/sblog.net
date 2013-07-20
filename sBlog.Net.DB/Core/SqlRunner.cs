e845f2934122e0c09fed96b7165ad49764e22320
Karthik Ananthapadmanaban <karthika25@gmail.com>
2013-07-20 00:42:15 -0400

    refactored the database deployer so that db is updated whenever a script is ran and not when all are ran

--- sBlog.Net.DB/Core/SqlRunner.cs
+++ sBlog.Net.DB/Core/SqlRunner.cs
@@ -24,20 +24,87 @@ namespace sBlog.Net.DB.Core
 
                 foreach (var sqlFile in sqlFiles)
                 {
-                    var contents = GetFileContents(sqlFile.ScriptPath);
-                    contents.ForEach(batchCommand =>
+                    if (HasScriptBeenRunAlready(sqlConnection, sqlFile))
                     {
-                        using (var sqlCommand = new SqlCommand(batchCommand, sqlConnection))
+                        SetSuccess(sqlFile);
+                        continue;
+                    }
+
+                    try
+                    {
+                        var contents = GetFileContents(sqlFile.ScriptPath);
+                        contents.ForEach(batchCommand =>
                         {
-                            sqlCommand.ExecuteNonQuery();
-                        }
-                    });
+                            using (var sqlCommand = new SqlCommand(batchCommand, sqlConnection))
+                            {
+                                sqlCommand.ExecuteNonQuery();
+                            }
+                        });
+
+                        InsertRecord(sqlConnection, sqlFile);
+                        SetSuccess(sqlFile);
+                    }
+                    catch (Exception e)
+                    {
+                        SetFailure(sqlFile, e);
+                    }
                 }
 
                 sqlConnection.Close();
             }
 
             return sqlFiles;
+        }
+
+        private static void SetSuccess(SchemaVersion sqlFile)
+        {
+            sqlFile.RunStatus = true;
+            sqlFile.ErrorMessage = string.Empty;
+            sqlFile.FullErrorMessage = string.Empty;
+        }
+
+        private static void SetFailure(SchemaVersion sqlFile, Exception e)
+        {
+            sqlFile.RunStatus = false;
+            sqlFile.ErrorMessage = e.Message;
+            sqlFile.FullErrorMessage = e.ToString();
+        }
+
+        private static void InsertRecord(SqlConnection sqlConnection, SchemaVersion schemaVersion)
+        {
+            const string insertSql = @"INSERT INTO dbo.[Schema] VALUES('{0}',{1},{2},{3},'{4}')";
+            var stmt = string.Format(insertSql, Path.GetFileName(schemaVersion.ScriptPath), schemaVersion.MajorVersion,
+                                     schemaVersion.MinorVersion, schemaVersion.ScriptVersion, DateTime.Now);
+            using (var insertCmd = new SqlCommand(stmt, sqlConnection))
+            {
+                insertCmd.ExecuteNonQuery();
+            }
+        }
+
+        private static bool HasScriptBeenRunAlready(SqlConnection sqlConnection, SchemaVersion schemaVersion)
+        {
+            SqlCommand selectCmd = null;
+            try
+            {
+                const string selectSql = @"SELECT COUNT(*) FROM dbo.[Schema] WHERE [MajorVersion] = {0} AND [MinorVersion] = {1} AND [ScriptVersion] = {2}";
+                var stmt = string.Format(selectSql, schemaVersion.MajorVersion, schemaVersion.MinorVersion,
+                                         schemaVersion.ScriptVersion);
+                selectCmd = new SqlCommand(stmt, sqlConnection);
+                var count = selectCmd.ExecuteScalar();
+                var result = int.Parse(count.ToString());
+                return result == 1;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                if (selectCmd != null)
+                {
+                    selectCmd.Dispose();
+                }
+            }
         }
 
         private static List<string> GetFileContents(string sqlFile)
--- sBlog.Net/Areas/Prepare/Models/DatabaseSetupModel.cs
+++ sBlog.Net/Areas/Prepare/Models/DatabaseSetupModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using sBlog.Net.DB.Services;
 using sBlog.Net.MetaData.MetaData;
 using System.ComponentModel.DataAnnotations;
 
@@ -12,6 +13,7 @@ namespace sBlog.Net.Areas.Prepare.Models
         public string Message { get; set; }
         public List<string> Scripts { get; set; }
 
+        public List<SchemaVersion> Results { get; set; }
         public string CompleteException { get; set; }
     }
 }
