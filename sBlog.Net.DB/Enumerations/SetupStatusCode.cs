9d995efabf6cc64335972bf1a3b4fea8a46b56c0
Karthik Ananthapadmanaban <karthika25@gmail.com>
2013-09-21 00:24:51 -0400

    add license info for some files missed

--- sBlog.Net.DB/Enumerations/SetupStatusCode.cs
+++ sBlog.Net.DB/Enumerations/SetupStatusCode.cs
@@ -1,4 +1,21 @@
-﻿namespace sBlog.Net.DB.Enumerations
+﻿#region Disclaimer/License Info
+
+/* *********************************************** */
+
+// sBlog.Net
+
+// sBlog.Net is a minimalistic blog engine software.
+
+// Homepage: http://sblogproject.net
+// Github: http://github.com/karthik25/sBlog.Net
+
+// This project is licensed under the BSD license.  
+// See the License.txt file for more information.
+
+/* *********************************************** */
+
+#endregion
+namespace sBlog.Net.DB.Enumerations
 {
     public enum SetupStatusCode
     {
--- sBlog.Net.DB/Services/SetupStatus.cs
+++ sBlog.Net.DB/Services/SetupStatus.cs
@@ -1,4 +1,21 @@
-﻿using sBlog.Net.DB.Enumerations;
+﻿#region Disclaimer/License Info
+
+/* *********************************************** */
+
+// sBlog.Net
+
+// sBlog.Net is a minimalistic blog engine software.
+
+// Homepage: http://sblogproject.net
+// Github: http://github.com/karthik25/sBlog.Net
+
+// This project is licensed under the BSD license.  
+// See the License.txt file for more information.
+
+/* *********************************************** */
+
+#endregion
+using sBlog.Net.DB.Enumerations;
 
 namespace sBlog.Net.DB.Services
 {
