695588b8957dcc5d69e4c080dce0eba455dda011
Karthik Ananthapadmanaban <karthika25@gmail.com>
2013-11-03 00:56:22 -0400

    more additions to the post view count plugin

---
+++ sBlog.Net.Plugins.PostViewCount/PostViewCountDataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace sBlog.Net.Plugins.PostViewCount
+{
+    public class PostViewCountDataContext : DbContext
+    {
+        public IDbSet<View> Views { get; set; }
+
+        public PostViewCountDataContext()
+            : base("Server=localhost;Database=sblog;user id=msuser1;password=msuser1;")
+        {
+            
+        }
+
+        public void InsertPostView(int postId, string postUrl)
+        {
+            var view = new View { PostId = postId, PostUrl = postUrl, ViewDate = DateTime.Now };
+            Views.Add(view);
+            SaveChanges();
+        }
+    }
+}
---
+++ sBlog.Net.Plugins.PostViewCount/View.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace sBlog.Net.Plugins.PostViewCount
+{
+    public class View
+    {
+        [Key]
+        public int ViewId { get; set; }
+        public int PostId { get; set; }
+        public DateTime ViewDate { get; set; }
+        public string PostUrl { get; set; }
+    }
+}
