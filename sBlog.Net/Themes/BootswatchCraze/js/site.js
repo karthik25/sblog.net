f5912fb71b3ab7c35e55647009d925a0a16f3348
Karthik Ananthapadmanaban <karthika25@gmail.com>
2014-02-18 21:27:35 -0500

    added a new theme w/ theme roller

---
+++ sBlog.Net/Themes/BootswatchCraze/js/site.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    var api = "http://api.bootswatch.com/3/";
+    $.ajax({
+        type: 'GET',
+        url: api,
+        dataType: 'json',
+        success: function (data) {
+            populateDropdown(data.themes);
+        }
+    });
+
+    $(document).on('change', '#bootswatch', function () {
+        var val = $('#bootswatch').val();
+        $('#bwLink').attr('href', val);
+    });
+});
+
+function populateDropdown(themes) {
+    $.each(themes, function () {
+        var theme = this;
+        $('#bootswatch').append($("<option></option>")
+         .attr("value", theme.cssCdn)
+         .text(theme.name));
+    });
+}
