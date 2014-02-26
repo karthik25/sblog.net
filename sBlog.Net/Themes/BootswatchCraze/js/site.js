ea72dfafbea6b5e7ae63543a2a059af2f0c19da0
Karthik Ananthapadmanaban <karthika25@gmail.com>
2014-02-25 20:03:13 -0500

    use local json for cdn paths

--- sBlog.Net/Themes/BootswatchCraze/js/site.js
+++ sBlog.Net/Themes/BootswatchCraze/js/site.js
@@ -1,17 +1,19 @@
 ﻿$(function () {
-    var api = "http://api.bootswatch.com/3/";
     $.ajax({
         type: 'GET',
         url: api,
         dataType: 'json',
         success: function (data) {
             populateDropdown(data.themes);
+            setupCookie();
+            setOption($.cookie('sblog_user_theme'));
         }
     });
 
     $(document).on('change', '#bootswatch', function () {
         var val = $('#bootswatch').val();
         $('#bwLink').attr('href', val);
+        updateCookie();
     });
 });
 
@@ -22,4 +24,15 @@ function populateDropdown(themes) {
          .attr("value", theme.cssCdn)
          .text(theme.name));
     });
+}
+
+function setupCookie() {
+    var userTheme = $.cookie('sblog_user_theme');
+    if (userTheme === null || userTheme === undefined) {
+        $.cookie('sblog_user_theme', $('#bootswatch').val(), { path: '/' });
+    }
+}
+
+function updateCookie() {
+    $.cookie('sblog_user_theme', $('#bootswatch').val(), { path: '/' });
 }
