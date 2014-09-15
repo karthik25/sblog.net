764b6df9f770f200ea6d3fa1e64ecda2872de1ea
Karthik Ananthapadmanaban <karthika25@gmail.com>
2014-09-14 22:46:45 -0400

    took care of some kinks, md working now! working on viewing md now

--- sBlog.Net/Scripts/markdown_init.js
+++ sBlog.Net/Scripts/markdown_init.js
@@ -1,6 +1,8 @@
 ﻿$(function () {
+    $('#ckeditor_toolbar').hide();
+
     $("textarea.mdd_editor").MarkdownDeep({
         help_location: "/Scripts/mdd_help.htm",
         ExtraMode: true
-    });
+    }).attr("rows", "10");
 })
